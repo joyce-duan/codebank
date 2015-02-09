592bc6eaeaa3c948de3a31874ebdcbf0a47bf2f7
Joyce <joyce-sf@users.noreply.github.com>
2015-02-08 19:07:41 -0800

    Create myutil.py

---
+++ util/myutil.py
@@ -0,0 +1,17 @@
+
+"""
+collection of utilities
+"""
+
+import re
+def camel_to_snake(column_name):
+    """
+    converts a string that is camelCase into snake_case
+    Example:
+        print camel_to_snake("javaLovesCamelCase")
+        > java_loves_camel_case
+    See Also:
+        http://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-camel-case
+    """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', column_name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
